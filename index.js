ec236ff0ce1db9f476e637daee0b2e3318209bfa
danilabonnie <danilagavilan@gmail.com>
2024-11-13 19:30:31 -0300

    bd anda mal waaaaaaaaaa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const fs = require ('fs');
 const cors = require ('cors');
 const app = express();
-const port = process.env.MYSQL_ADDON_PORT || 3000;
+const port =  process.env.MYSQL_ADDON_PORT || 3000;
 
 const db = require ('./db/conexion')
 const dotenv = require ('dotenv/config')
